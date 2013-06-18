a552305f69c7c8364d43fd295ce3b3e2da8f950b
Andrey Sokolov <a.sokolov@spbu.ru>
2013-06-18 23:54:04 +0400

    Main code rev. 1726
    Code cleanup of ternary conditions and removed trailing spaces.

--- CJDB/lib/CUFTS/CJDB/Controller/Root.pm
+++ CJDB/lib/CUFTS/CJDB/Controller/Root.pm
@@ -49,7 +49,7 @@ sub begin : Private {
 
 sub site :Chained('/') :PathPart('') :CaptureArgs(1) {
     my ($self, $c, $site_key) = @_;
-    
+
     my $site = CUFTS::DB::Sites->search( key => $site_key )->first;
     if ( !defined($site) ) {
         die("Unrecognized site key: $site_key");
@@ -62,27 +62,27 @@ sub site :Chained('/') :PathPart('') :CaptureArgs(1) {
 
     # Set up site specific CSS file if it exists
     my $site_css =   '/sites/' . $site->id . "/static/css/${box}/cjdb.css";
-                  
+
     if ( -e ($c->config->{root} . $site_css) ) {
         $c->stash->{site_css_file} = $c->uri_for( $site_css );
     }
 
     # Get the current user for the stash if they have logged in
     if ( defined( $c->session->{ $c->stash->{current_site}->id }->{current_account_id} ) ) {
-        $c->stash->{current_account} = CJDB::DB::Accounts->retrieve( 
-            $c->session->{ $c->stash->{current_site}->id }->{current_account_id} 
+        $c->stash->{current_account} = CJDB::DB::Accounts->retrieve(
+            $c->session->{ $c->stash->{current_site}->id }->{current_account_id}
         );
     }
 
     $self->_cache_local_resources($c);
-    
+
     # Store previous action/arguments/parameters data
     if ( $c->req->action !~ /account/ && $c->req->action !~ /ajax/ ) {
         $c->session->{prev_uri} = $c->req->uri;
     }
 
     $c->stash->{url_base} = $c->request->base . $site->key;
-    $c->stash->{additional_template_paths} = [ $c->config->{root} . '/sites/' . $site->id . "/${box}" ];    
+    $c->stash->{additional_template_paths} = [ $c->config->{root} . '/sites/' . $site->id . "/${box}" ];
     $c->stash->{image_dir} = $c->request->base . '/static/images/';
     $c->stash->{css_dir}   = $c->request->base . '/static/css/';
     $c->stash->{js_dir}    = $c->request->base . '/static/js/';
@@ -105,32 +105,28 @@ sub _cache_local_resources {
             my $resource_id = $resource->id;
             my $global_resource = $resource->resource;
 
-            $resources_display{$resource_id}->{cjdb_note} = hascontent($resource->cjdb_note)
-                                                            ? $resource->cjdb_note
-                                                            : defined($global_resource)
-                                                            ? $global_resource->cjdb_note
-                                                            : '';
-                                                            
-            $resources_display{$resource_id}->{name} = hascontent($resource->name) 
-                                                       ? $resource->name
-                                                       : defined($global_resource)
-                                                       ? $global_resource->name 
-                                                       : '';
-                                                       
+            $resources_display{$resource_id}->{cjdb_note} = hascontent($resource->cjdb_note) ? $resource->cjdb_note
+                                                          : defined($global_resource)        ? $global_resource->cjdb_note
+                                                          :                                    '';
+
+            $resources_display{$resource_id}->{name} = hascontent($resource->name) ? $resource->name
+                                                     : defined($global_resource)   ? $global_resource->name
+                                                     :                               '';
+
             if (!$c->stash->{current_site}->cjdb_display_db_name_only) {
-                my $provider = hascontent($resource->provider) 
+                my $provider = hascontent($resource->provider)
                                ? $resource->provider
                                : defined($global_resource)
-                               ? $global_resource->provider 
+                               ? $global_resource->provider
                                : '';
                 $resources_display{$resource_id}->{name} .= " - ${provider}";
             }
         }
-        
+
         $c->stash->{resources_display} = \%resources_display;
         $c->cache->set( "resources_display_${site_id}", \%resources_display );
     }
-    
+
 }
 
 
@@ -142,15 +138,15 @@ sub indexy :Chained('site') :PathPart('') Args(0) {
 
 sub set_box :Chained('site') :PathPart('set_box') :Args(1) {
     my ( $self, $c, $box ) = @_;
-    
+
     $c->session->{sandbox} = $box eq 'sandbox' ? 1 : 0;
-    
+
     $c->redirect( $c->uri_for_site( $c->controller->action_for('indexy') ) );
 }
 
 sub site_files :Chained('site') :PathPart('sites') :Args(3) {
     my ( $self, $c, @args ) = @_;
-    
+
     my $path = $c->config->{root} . '/sites/' . join('/', @args);
     warn($path);
     $c->serve_static_file($path);
@@ -171,7 +167,7 @@ sub default :Path {
 
 sub favicon : Path('favicon.ico') {
     my ( $self, $c ) = @_;
- 
+
     $c->response->body('');
     $c->response->status(404);
     $c->detach();
