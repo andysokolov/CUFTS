b13f85e4c956aa69d576211c599ca35f38bcec59
Andrey Sokolov <a.sokolov@spbu.ru>
2013-12-07 16:51:34 +0400

    Added some helper VMETHODS to TT
    Main code rev. 1817

--- CRDB4/lib/CUFTS/CRDB4/View/TT.pm
+++ CRDB4/lib/CUFTS/CRDB4/View/TT.pm
@@ -25,6 +25,28 @@ $Template::Stash::LIST_OPS->{ in } = sub {
 	}
 };
 
+$Template::Stash::SCALAR_OPS->{force_list} = sub {
+    return [ shift ];
+};
+
+$Template::Stash::LIST_OPS->{force_list} = sub {
+    return @_;
+};
+
+$Template::Stash::HASH_OPS->{force_list} = sub {
+    return [ shift ];
+};
+
+
+$Template::Stash::SCALAR_OPS->{substr} = sub { my ($scalar, $offset, $length) = @_; return defined($length) ? substr($scalar, $offset, $length) : substr($scalar, $offset); };
+$Template::Stash::SCALAR_OPS->{ceil} = sub { return (int($_[0]) < $_[0]) ? int($_[0] + 1) : int($_[0]) };  # Cheap
+$Template::Stash::LIST_OPS->{map_join} = sub {
+	my ($list, $field, $join) = @_;
+	return join( $join, map { blessed($_) ? $_->$field : $_->{$field} } @$list );
+};
+
+$Template::Stash::SCALAR_OPS->{uri_escape} = sub { my $text = shift; $text =~ s/([^a-zA-Z0-9_.-])/uc sprintf("%%%02x",ord($1))/eg; return $text; };
+
 =head1 NAME
 
 CUFTS::CRDB4::View::TT - TT View for CUFTS::CRDB4
