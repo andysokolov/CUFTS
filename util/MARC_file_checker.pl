fb5dfa9b8a89eff34d4db44930233004a4a8a00e
Andrey Sokolov <a.sokolov@spbu.ru>
2013-12-13 21:04:04 +0400

    Added better feedback in the MARC test script
    Main code rev. 1822

--- util/MARC_file_checker.pl
+++ util/MARC_file_checker.pl
@@ -7,6 +7,8 @@ use strict;
 use MARC::Batch;
 use MARC::Record;
 use Business::ISSN;
+use String::Util qw(hascontent trim);
+
 
 #$MARC::Record::DEBUG = 1;
 
@@ -17,15 +19,19 @@ my $count = 0;
 while (my $record = $batch->next()) {
 	$count++;
 
-	my $title = $record->field('245')->as_string;
+	my $title = $record->title;
 	my @errs;
+
+	if ( !hascontent($title) ) {
+		push @errs, "Title not found in 245 field."
+	}
 
 	foreach my $issn_field ($record->field('022')) {
 		foreach my $subfield ('a'..'z') {
-			my $issn = $issn_field->subfield($subfield);
-						
+			my $issn = clean_issn( $issn_field->subfield($subfield) );
+
 			next unless $issn =~ /\S/;  # Make sure it's not all whitespace
-			
+
 			if ($issn !~ /^\d{4}\-?\d{3}[\dXx]$/) {
 				push @errs, "ISSN does not match ISSN pattern: $issn";
 			} elsif ($subfield ne 'y' && !Business::ISSN::is_valid_checksum($issn)) {
@@ -33,10 +39,10 @@ while (my $record = $batch->next()) {
 			}
 		}
 	}
-	
+
 	if (scalar(@errs)) {
 		print "Record $count\n";
-		print "$title\n";
+		print "$title\n" || "Missing title\n";
 		print join "\n", @errs;
 		print "\n";
 		print "------------------------------------------\n";
@@ -44,14 +50,13 @@ while (my $record = $batch->next()) {
 
 }
 
-sub clean_issn {
-        my ($self, $issn) = @_;
-        
-        $issn =~ s/-//;
-        $issn = uc($issn);
-        $issn =~ s/^\s+//;
-        $issn =~ s/\s+$//;
- 
-        return $issn;
-}        
+print "Parser found $count records.\n";
 
+sub clean_issn {
+        my $issn = shift;
+
+        $issn =~ s/-//;
+        $issn = trim(uc($issn));
+
+        return $issn;
+}
