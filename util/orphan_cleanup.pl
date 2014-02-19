8a8814d1d69acf2b733f8f41db3d9072852fe094
Andrey Sokolov <a.sokolov@spbu.ru>
2014-02-19 22:27:11 +0400

    Updated orphan cleanup script to use new DBIC schema
    Main code rev. 1855

--- util/orphan_cleanup.pl
+++ util/orphan_cleanup.pl
@@ -4,22 +4,10 @@ use Data::Dumper;
 
 use lib qw(lib);
 
-use CJDB::DB::DBI;
-use CUFTS::DB::DBI;
-
-use CUFTS::DB::Resources;
-use CUFTS::DB::Journals;
-use CUFTS::DB::JournalsAuth;
-
-use CJDB::DB::Journals;
-use CJDB::DB::Tags;
-
-use CUFTS::DB::ERMMain;
-use CUFTS::DB::ERMCounterTitles;
+use CUFTS::Config;
 
 use Getopt::Long;
 use Log::Log4perl qw(:easy);
-
 
 my %options;
 GetOptions( \%options, 'save', 'all', 'unlinked_jas', 'journal_links', 'orphaned_titles', 'orphaned_issns' );
@@ -50,36 +38,33 @@ else {
 
 
 sub unlinked_journal_auth {
+
     $logger->info('Looking for Journal Auth records that are not linked to anything.');
-    my $ja_iter = CUFTS::DB::JournalsAuth->retrieve_all();
+
+    my $schema = CUFTS::Config::get_schema();
+    $schema->txn_begin if $options{save};
+
+    my $ja_rs = $schema->resultset('JournalsAuth');
+
     my $remove_count = 0;
-    while ( my $ja = $ja_iter->next ) {
+    while ( my $ja = $ja_rs->next ) {
         my $ja_id = $ja->id;
 
         # Check for links to journals and local_journals
 
-        my $count = CUFTS::DB::Journals->count_search({ journal_auth => $ja_id });
-        next if $count;
+        next if $ja->global_journals->count();
+        next if $ja->local_journals->count();
 
-        $count = CUFTS::DB::LocalJournals->count_search({ journal_auth => $ja_id });
-        next if $count;
+        next if $ja->cjdb_journals->count();
+        next if $ja->cjdb_tags->count();
 
-        $count = CJDB::DB::Journals->count_search({ journals_auth => $ja_id });
-        next if $count;
-
-        $count = CJDB::DB::Tags->count_search({ journals_auth => $ja_id });
-        next if $count;
-
-        $count = CUFTS::DB::ERMMain->count_search({ journal_auth => $ja_id });
-        next if $count;
-
-        $count = CUFTS::DB::ERMCounterTitles->count_search({ journal_auth => $ja_id });
-        next if $count;
+        next if $ja->erm_mains->count();
+        next if $ja->erm_counter_titles->count();
 
         if ( $options{save} ) {
             $logger->info('Deleting: ', $ja->title, ' (', $ja_id, ')' );
-            CUFTS::DB::JournalsAuthTitles->search({ journal_auth => $ja_id })->delete_all;
-            CUFTS::DB::JournalsAuthISSNs->search({ journal_auth => $ja_id })->delete_all;
+            $ja->titles->delete_all();
+            $ja->issns->delete_all();
             $ja->delete;
         }
         else {
@@ -91,42 +76,41 @@ sub unlinked_journal_auth {
 
     $logger->info('Total of ', $remove_count, ' unused journal authority records found.');
 
-    if ( $options{save} ) {
-        CUFTS::DB::DBI->dbi_commit();
-    }
+    $schema->txn_commit if $options{save};
 }
 
 sub fix_journal_links {
     $logger->info('Looking for journal links to Journal Auth records that no longer exist.');
-    my $journal_iter = CUFTS::DB::Journals->search({ journal_auth => { '!=' => undef } });
-    $remove_count = 0;
-    while ( my $journal = $journal_iter->next ) {
-        my $ja = $journal->journal_auth;
-        next if !defined($ja);
-        next if defined($ja) && defined($ja->id) && defined($ja->title);
 
+    my $schema = CUFTS::Config::get_schema();
+    $schema->txn_begin if $options{save};
+
+    my $journal_rs = $schema->resultset('GlobalJournals')->search({ 'me.journal_auth' => { '!=' => undef }, 'journal_auth.id' => undef }, { join => 'journal_auth' });
+    $remove_count = 0;
+    while ( my $journal = $journal_rs->next ) {
         if ( $options{save} ) {
-            $logger->info('Clearing link: ', $journal->title, ' (', $journal->id, ') to journal_auth id: ', $ja_id );
-            $journal->journal_auth(undef);
-            $journal->update;
+            $logger->info('Clearing link: ', $journal->title, ' (', $journal->id, ') to journal_auth id: ', $journal->get_column('journal_auth') );
+            $journal->update({ journal_auth => undef });
         }
         else {
-            $logger->info('Found: ', $journal->title, ' (', $journal->id, ') to journal_auth id: ', $ja_id );
+            $logger->info('Found: ', $journal->title, ' (', $journal->id, ') to journal_auth id: ', $journal->get_column('journal_auth') );
         }
 
         $remove_count++;
     }
 
     $logger->info('Total of ', $remove_count, ' links to missing journal authority records found.');
-    if ( $options{save} ) {
-        CUFTS::DB::DBI->dbi_commit();
-    }
+    $schema->txn_commit if $options{save};
 }
 
 sub orphaned_issns {
     $logger->info('Looking for JournalAuthISSNs that no longer have a matching JournalAuth record.');
-    my $issn_iter = CUFTS::DB::JournalsAuthISSNs->search();
-    while ( my $issn = $issn_iter->next ) {
+
+    my $schema = CUFTS::Config::get_schema();
+    $schema->txn_begin if $options{save};
+
+    my $issn_rs = $schema->resultset('JournalsAuthISSNs')->search({ 'journal_auth_left.id' => undef }, { join => 'journal_auth_left'} );
+    while ( my $issn = $issn_rs->next ) {
         my $ja = $issn->journal_auth;
         next if defined($ja) && defined($ja->id) && defined($ja->title);
         if ( $options{save} ) {
@@ -137,15 +121,18 @@ sub orphaned_issns {
             $logger->info("Found an ISSN attached to a missing record. ISSN id: " . $issn->issn . ', journal auth id: ' . $issn->journal_auth );
         }
     }
-    if ( $options{save} ) {
-        CUFTS::DB::DBI->dbi_commit();
-    }
+
+    $schema->txn_commit if $options{save};
 }
 
 sub orphaned_titles {
     $logger->info('Looking for JournalAuthTitles that no longer have a matching JournalAuth record.');
-    my $title_iter = CUFTS::DB::JournalsAuthTitles->search();
-    while ( my $title = $title_iter->next ) {
+
+    my $schema = CUFTS::Config::get_schema();
+    $schema->txn_begin if $options{save};
+
+    my $title_rs = $schema->resultset('JournalsAuthTitles')->search({ 'journal_auth_left.id' => undef }, { join => 'journal_auth_left'} );
+    while ( my $title = $title_rs->next ) {
         my $ja = $title->journal_auth;
         next if defined($ja) && defined($ja->id) && defined($ja->title);
         if ( $options{save} ) {
@@ -156,7 +143,6 @@ sub orphaned_titles {
             $logger->info("Found a title attached to a missing record. Title id: " . $title->title . ', journal auth id: ' . $title->journal_auth );
         }
     }
-    if ( $options{save} ) {
-        CUFTS::DB::DBI->dbi_commit();
-    }
+
+    $schema->txn_commit if $options{save};
 }
