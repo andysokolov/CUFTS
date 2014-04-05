27a0182073983952b6b05b0dc0d8f7c6c71a1aa7
Andrey Sokolov <a.sokolov@spbu.ru>
2014-04-05 10:19:45 +0400

    Update FCGI script
    Main code rev. 1894

--- MaintTool/script/cufts_mainttool_fastcgi.pl
+++ MaintTool/script/cufts_mainttool_fastcgi.pl
@@ -1,37 +1,10 @@
-#!/usr/local/bin/perl -w
+#!/usr/bin/env perl
 
-BEGIN { $ENV{CATALYST_ENGINE} ||= 'FastCGI' }
-
-use strict;
-use warnings;
-use Getopt::Long;
-use Pod::Usage;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
-use CUFTS::MaintTool;
 
-my $help = 0;
-my ( $listen, $nproc, $pidfile, $manager, $detach );
- 
-GetOptions(
-    'help|?'      => \$help,
-    'listen|l=s'  => \$listen,
-    'nproc|n=i'   => \$nproc,
-    'pidfile|p=s' => \$pidfile,
-    'manager|M=s' => \$manager,
-    'daemon|d'    => \$detach,
-);
-
-pod2usage(1) if $help;
-
-CUFTS::MaintTool->run( 
-    $listen, 
-    {   nproc   => $nproc,
-        pidfile => $pidfile, 
-        manager => $manager,
-        detach  => $detach,
-    }
-);
+use Catalyst::ScriptRunner;
+Catalyst::ScriptRunner->run('CUFTS::MaintTool', 'FastCGI');
 
 1;
 
@@ -42,35 +15,37 @@ cufts_mainttool_fastcgi.pl - Catalyst FastCGI
 =head1 SYNOPSIS
 
 cufts_mainttool_fastcgi.pl [options]
- 
+
  Options:
-   -? -help      display this help and exits
-   -l -listen    Socket path to listen on
+   -? --help      display this help and exit
+   -l --listen   socket path to listen on
                  (defaults to standard input)
                  can be HOST:PORT, :PORT or a
                  filesystem path
-   -n -nproc     specify number of processes to keep
+   -n --nproc    specify number of processes to keep
                  to serve requests (defaults to 1,
-                 requires -listen)
-   -p -pidfile   specify filename for pid file
-                 (requires -listen)
-   -d -daemon    daemonize (requires -listen)
-   -M -manager   specify alternate process manager
+                 requires --listen)
+   -p --pidfile  specify filename for pid file
+                 (requires --listen)
+   -d --daemon   daemonize (requires --listen)
+   -M --manager  specify alternate process manager
                  (FCGI::ProcManager sub-class)
                  or empty string to disable
+   -e --keeperr  send error messages to STDOUT, not
+                 to the webserver
+   --proc_title  Set the process title (if possible)
 
 =head1 DESCRIPTION
 
-Run a Catalyst application as fastcgi.
+Run a Catalyst application as FastCGI.
 
-=head1 AUTHOR
+=head1 AUTHORS
 
-Sebastian Riedel, C<sri@oook.de>
-Maintained by the Catalyst Core Team.
+Catalyst Contributors, see Catalyst.pm
 
 =head1 COPYRIGHT
 
-This library is free software, you can redistribute it and/or modify
+This library is free software. You can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
