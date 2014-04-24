b02e966fa9d7f9800ab82427ee3f492c31808bb0
Andrey Sokolov <a.sokolov@spbu.ru>
2014-04-24 23:07:01 +0400

    Fixed up KBARTJournal commits
    Main code rev. 1910

---
+++ lib/CUFTS/Resources/KBARTJournal.pm
@@ -0,0 +1,64 @@
+## CUFTS::Resources::KBARTJournal
+##
+## Copyright Todd Holbrook - Simon Fraser University (2003)
+##
+## This file is part of CUFTS.
+##
+## CUFTS is free software; you can redistribute it and/or modify it under
+## the terms of the GNU General Public License as published by the Free
+## Software Foundation; either version 2 of the License, or (at your option)
+## any later version.
+##
+## CUFTS is distributed in the hope that it will be useful, but WITHOUT ANY
+## WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+## FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+## details.
+##
+## You should have received a copy of the GNU General Public License along
+## with CUFTS; if not, write to the Free Software Foundation, Inc., 59
+## Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+package CUFTS::Resources::KBARTJournal;
+
+use base qw(CUFTS::Resources::Base::KBART);
+
+use CUFTS::Exceptions;
+use CUFTS::Util::Simple;
+
+use strict;
+
+sub build_linkJournal {
+    my ( $class, $schema, $records, $resource, $site, $request ) = @_;
+
+    defined($records) && scalar(@$records) > 0
+        or return [];
+    defined($resource)
+        or CUFTS::Exception::App->throw('No resource defined in build_linkJournal');
+    defined($site)
+        or CUFTS::Exception::App->throw('No site defined in build_linkJournal');
+    defined($request)
+        or CUFTS::Exception::App->throw('No request defined in build_linkJournal');
+
+    my @results;
+
+    foreach my $record (@$records) {
+        next if is_empty_string( $record->journal_url );
+
+        my $result = new CUFTS::Result( $record->journal_url );
+        $result->record($record);
+
+        push @results, $result;
+    }
+
+    return \@results;
+}
+
+sub can_getFulltext {
+    return 0;
+}
+
+sub can_getTOC {
+    return 0;
+}
+
+1;
