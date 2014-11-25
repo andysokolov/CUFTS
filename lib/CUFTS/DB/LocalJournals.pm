340723592b83058fe006ae6fcaf0fb8832cc9172
Andrey Sokolov <a.sokolov@spbu.ru>
2014-11-25 23:06:07 +0300

    Added quick way to check if a local journal has an overlay field set.
    Main code rev. 2033

--- lib/CUFTS/DB/LocalJournals.pm
+++ lib/CUFTS/DB/LocalJournals.pm
@@ -8,7 +8,7 @@
 ## the terms of the GNU General Public License as published by the Free
 ## Software Foundation; either version 2 of the License, or (at your option)
 ## any later version.
-## 
+##
 ## CUFTS is distributed in the hope that it will be useful, but WITHOUT ANY
 ## WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 ## FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
@@ -29,6 +29,7 @@ use CUFTS::DB::LocalResources;
 use CUFTS::DB::ERMMain;
 
 use CUFTS::Util::Simple;
+use String::Util qw(hascontent trim);
 
 __PACKAGE__->table('local_journals');
 __PACKAGE__->columns(Primary => 'id');
@@ -57,7 +58,7 @@ __PACKAGE__->columns(All => qw(
     embargo_days
     journal_auth
 
-    db_identifier   
+    db_identifier
     toc_url
     journal_url
     urlbase
@@ -67,9 +68,9 @@ __PACKAGE__->columns(All => qw(
     current_years
     cjdb_note
     coverage
-    
+
     local_note
-    
+
     erm_main
 
     created
@@ -87,7 +88,7 @@ __PACKAGE__->has_a('erm_main', 'CUFTS::DB::ERMMain');
 
 sub normalize_column_values {
     my ($self, $values) = @_;
-    
+
     # Check ISSNs for dashes and strip them out
 
     if (exists($values->{'issn'}) && defined($values->{'issn'}) && $values->{'issn'} ne '') {
@@ -99,9 +100,9 @@ sub normalize_column_values {
         $values->{'e_issn'} =~ s/(\d{4})\-?(\d{3}[\dxX])/$1$2/ or
             $self->_croak('e_issn is not valid: ' . $values->{'e_issn'});
     }
-    
+
     # Set default dates if they're just years or years/months
-    
+
     $values->{ft_start_date}  = set_default_dates($values->{ft_start_date}, 'start')  if exists($values->{ft_start_date});
     $values->{cit_start_date} = set_default_dates($values->{cit_start_date}, 'start') if exists($values->{cit_start_date});
 
@@ -113,6 +114,48 @@ sub normalize_column_values {
 
 sub global_join_field {
     return 'journal';
+}
+
+sub has_overlay {
+    my $self = shift;
+
+    my @check_columns = qw(
+        vol_cit_start
+        vol_cit_end
+        vol_ft_start
+        vol_ft_end
+        iss_cit_start
+        iss_cit_end
+        iss_ft_start
+        iss_ft_end
+        cit_start_date
+        cit_end_date
+        ft_start_date
+        ft_end_date
+        embargo_months
+        embargo_days
+        journal_auth
+
+        db_identifier
+        toc_url
+        journal_url
+        urlbase
+        publisher
+        abbreviation
+        current_months
+        current_years
+        cjdb_note
+        coverage
+
+        local_note
+    );
+
+    foreach my $column (@check_columns) {
+        warn( $column . ' ' . hascontent($self->$column) );
+        return 1 if hascontent($self->$column);
+    }
+
+    return 0;
 }
 
 1;
